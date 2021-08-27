9afeb0c28b4f4102d6be20ad81b0bf5ec9b21ac2
yuyi <38847066+ys558@users.noreply.github.com>
2021-08-27 21:58:40 +0800

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+  return <div className="App">
+      <p>{count}</p>
+      <button onClick={()=> setCount(count + 1 )}>+</button>
+      <button onClick={()=> setCount(count-1)}>-</button>
+    </div>
+
+}
+
+export default App;
